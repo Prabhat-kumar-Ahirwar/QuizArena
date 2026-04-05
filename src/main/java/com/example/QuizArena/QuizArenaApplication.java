f4fe09cb895694937eea6686c5b7878bfdeb9da3
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-04-05 10:33:08 +0530

    Initial commit

---
+++ src/main/java/com/example/QuizArena/QuizArenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuizArena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuizArena/QuizArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuizArena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
