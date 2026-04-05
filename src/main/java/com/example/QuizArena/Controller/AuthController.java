f5a183da284b039915a52734606127446b683cda
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-04-05 19:02:23 +0530

    adding security with jwt

--- src/main/java/com/example/QuizArena/Controller/AuthController.java
+++ src/main/java/com/example/QuizArena/Controller/AuthController.java
@@ -1,9 +1,14 @@
 package com.example.QuizArena.Controller;
 
+import com.example.QuizArena.JWT.JwtFilter;
+import com.example.QuizArena.JWT.JwtUtil;
 import com.example.QuizArena.Model.User;
 import com.example.QuizArena.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +21,33 @@ public class AuthController {
     private UserRepository userRepo;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
 
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),
+                        user.getPassword()
+                )
+        );
+
+        if (auth.isAuthenticated()) {
+            return jwtUtil.generateToken(user.getUsername());
+        }
+
+        return "Invalid credentials";
     }
 
 
